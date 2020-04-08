22633848e2e358d59da44d47202736db32f08de2
Fatih Taskent <fatih-taskent@hotmail.com>
2020-04-08 15:42:03 +0200

    add router navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { CountryPanelComponent } from './country-panel/country-panel.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavComponent } from './nav/nav.component';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './routes';
 
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   declarations: [
     AppComponent,
-    CountryPanelComponent
+    DashboardComponent,
+    NavComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
